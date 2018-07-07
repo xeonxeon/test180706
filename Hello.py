8ec92e37a501c2feedc4746a1069f5810a982673
xeonxeon <xeon444xeon@gmail.com>
2018-07-07 17:46:50 +0900

    ADD file hello.py

--- Hello.py
+++ Hello.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
 print('Hello Python!')
+print('Hello Python!')
+print('Hello Python!')
+print('Hello Python!')
+print('Hello Python!')
+print('Hello Python!')
+print('Hello Python!')
+print('Hello Python!')
+print('Hello Python!')
+print('Hello Python!')
